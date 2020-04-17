139aa36141d7d459c92447e562e3c19aa536801c
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-17 14:53:43 -0300

    Concluindo os testes de validar email

--- src/test/java/com/myapps/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/myapps/minhasfinancas/service/UsuarioServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,16 +14,18 @@ import com.myapps.minhasfinancas.exception.RegraNegocioException;
 import com.myapps.minhasfinancas.model.repository.IUsuarioRepository;
 import com.myapps.minhasfinancas.service.impl.UsuarioServiceImpl;
 
+@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
 	IUsuarioService usuarioService;
+
+	@MockBean
 	IUsuarioRepository usuarioRepository;
 
 	@BeforeEach
 	public void setUp() {
-		usuarioRepository = Mockito.mock(IUsuarioRepository.class);
 		usuarioService = new UsuarioServiceImpl(usuarioRepository);
 	}
 
