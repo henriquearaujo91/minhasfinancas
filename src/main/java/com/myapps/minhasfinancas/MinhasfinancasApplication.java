af4dad89697cd2ef2a8c96c901b06c0510b88e6b
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-21 13:14:18 -0300

    Habilitando o CORS e Conectando o Login com a API REST

--- src/main/java/com/myapps/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/myapps/minhasfinancas/MinhasfinancasApplication.java
@@ -2,9 +2,19 @@ package com.myapps.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc
+public class MinhasfinancasApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")// LIBERA TODAS AS URLS DA APLICACAO
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");// LIBERA PARA OS SEGUINTES METODOS
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
